366039c4df31f8abb2905d81a5fef79721e50690
urchade <urchadcassim@outlook.fr>
2020-05-16 14:04:36 +0200

    add files

---
+++ README.md
@@ -0,0 +1,5 @@
+# Original-LSTM
+
+Implementation of the original LSTM (without forget gate) (Hochreiter and Schmidhuber, 1997)
+
+The paper: https://www.mitpressjournals.org/doi/10.1162/neco.1997.9.8.1735
---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="lstm-variants",
+    version="0.0.1",
+    author="Urchade Zaratiana",
+    author_email="urchade.zaratiana@gmail.com",
+    description="Long-Short Term Memory variants",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/urchade/lstm-variants.git",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
